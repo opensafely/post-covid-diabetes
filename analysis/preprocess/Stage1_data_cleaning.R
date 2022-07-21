1f8727d6e1f23e8f53a485350d9bd8c1f2a0ee0e
RochelleKnight <rochelle.knight@bristol.ac.uk>
2022-07-21 12:22:12 +0100

    change region reference to East

--- analysis/preprocess/Stage1_data_cleaning.R
+++ analysis/preprocess/Stage1_data_cleaning.R
@@ -102,7 +102,7 @@ stage1 <- function(cohort_name, group){
     input$cov_cat_deprivation <- ordered(input$cov_cat_deprivation, levels = c("1-2 (most deprived)","3-4","5-6","7-8","9-10 (least deprived)"))
       
     ## cov_cat_region
-    input$cov_cat_region <- relevel(input$cov_cat_region, ref = "London")
+    input$cov_cat_region <- relevel(input$cov_cat_region, ref = "East")
     
     ## cov_cat_smoking_status
     levels(input$cov_cat_smoking_status) <- list("Ever smoker" = "E", "Missing" = "M", "Never smoker" = "N", "Current smoker" = "S")
@@ -241,7 +241,7 @@ stage1 <- function(cohort_name, group){
                       filter(cov_cat_region != "Missing")%>%
                       mutate(cov_cat_region = as.factor(cov_cat_region))
     
-    input$cov_cat_region <- relevel(input$cov_cat_region, ref = "London")
+    input$cov_cat_region <- relevel(input$cov_cat_region, ref = "East")
     cohort_flow[nrow(cohort_flow)+1,] <- c(nrow(input),as.numeric(cohort_flow[7,1]) - nrow(input), "Criteria 6 (Inclusion): Known region")
     
     #Exclusion criteria 6: SARS-CoV-2 infection recorded prior to the start of follow-up

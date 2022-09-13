ddab38788558112e9bc9204002549ab67e8f3bf0
Kurt Taylor <kurt.taylor@bristol.ac.uk>
2022-09-13 10:10:56 +0100

    update if statement

--- analysis/model/04_01_(a)_cox_fit_model.R
+++ analysis/model/04_01_(a)_cox_fit_model.R
@@ -166,7 +166,7 @@ fit_model_reducedcovariates <- function(event,subgroup,stratify_by_subgroup,stra
       }
       
       #If subgroup is not sex then add sex into formula - also do not add sex if event name is gestational dm (analyses performed in women only)
-      if ((startsWith(subgroup,"sex"))==F & (!"sex" %in% covariates_excl_region_sex_age) & (event_name != "gestationaldm")){
+      if (!startsWith(subgroup,"sex") & !startsWith(subgroup,"aer") & (!"sex" %in% covariates_excl_region_sex_age) & (event_name != "gestationaldm")){
         surv_formula <- paste(surv_formula, "sex", sep="+")
       }
       

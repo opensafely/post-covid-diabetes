272396b172bfcdc44b8d4acfe0e5604ad5ec7596
Kurt Taylor <kurt.taylor@bristol.ac.uk>
2022-11-22 15:42:45 +0000

    change minimum from 5 to 6

--- analysis/descriptives/diabetes-follow-up-analysis.R
+++ analysis/descriptives/diabetes-follow-up-analysis.R
@@ -187,16 +187,16 @@ ggplot(input_hist_hosp, aes(x=days_t2dm_diag_post_covid)) + geom_histogram(binwi
 ggplot2::ggsave(paste0("output/review/descriptives/days_t2dm_diag_post_hosp_covid_histogram_",cohort_name,".png"), height = 200, width = 300, unit = "mm", dpi = 600, scale = 1)
 
 
-# Add plots with minimum counts set to 5 ----------------------------------
+# Add plots with minimum counts set to 6 ----------------------------------
 
 input_hist_plot <- input_hist %>%
   dplyr::count(bin = floor(days_t2dm_diag_post_covid)) %>%
-  mutate(n = pmax(5, n))
+  mutate(n = pmax(6, n))
 
 input_hist_hosp_plot <- input_hist %>% 
   dplyr::filter(sub_cat_covid19_hospital == "hospitalised") %>%
   dplyr::count(bin = floor(days_t2dm_diag_post_covid)) %>%
-  mutate(n = pmax(5, n))
+  mutate(n = pmax(6, n))
 
 ggplot(input_hist_plot, aes(bin, n)) +
   geom_col() +

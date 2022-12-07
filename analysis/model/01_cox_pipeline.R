6741c474ca0812996b64ab3384b23fd3c32dad0c
Kurt Taylor <kurt.taylor@bristol.ac.uk>
2022-12-07 08:46:21 +0000

    update analyses to run for day zero

--- analysis/model/01_cox_pipeline.R
+++ analysis/model/01_cox_pipeline.R
@@ -32,7 +32,7 @@ library(matrixStats)
 args = commandArgs(trailingOnly=TRUE)
 
 if(length(args)==0){
-  event_name="t2dm_extended_follow_up"
+  event_name="t2dm"
   cohort="prevax"
 }else{
   event_name  = args[[1]]
@@ -84,7 +84,7 @@ analyses_to_run <- rbind(analyses_to_run, analyses_to_run_normal_timepoint)
 rm(analyses_to_run_normal_timepoint)
 
 # Add day zero analyses
-day_zero_analyses <- analyses_to_run %>% filter(subgroup == "main")
+day_zero_analyses <- analyses_to_run 
 day_zero_analyses$reduced_timepoint <- paste0("day_zero_",day_zero_analyses$reduced_timepoint)
 analyses_to_run <- rbind(analyses_to_run, day_zero_analyses)
 rm(day_zero_analyses)

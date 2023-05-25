654ebf01184e87eb8bd78437d5a27c0ee3ae5266
RochelleKnight <rochelle.knight@bristol.ac.uk>
2023-05-25 09:59:03 +0100

    Update TABLE-S8-t2dm-estimated-excess-events-at-28-weeks.R

--- analysis/figures/supplementary/TABLE-S8-t2dm-estimated-excess-events-at-28-weeks.R
+++ analysis/figures/supplementary/TABLE-S8-t2dm-estimated-excess-events-at-28-weeks.R
@@ -7,7 +7,8 @@ library(tidyr)
 #results_dir <- "C:/Users/zy21123/OneDrive - University of Bristol/Documents/OpenSAFELY/Outputs/release/"
 
 #Read in AER
-df <- readr::read_csv("C:/Users/zy21123/OneDrive - University of Bristol/Documents - grp-EHR/Projects/post-covid-diabetes/results/model/AER/AER_compiled_results_extended.csv")
+#df <- readr::read_csv("C:/Users/zy21123/OneDrive - University of Bristol/Documents - grp-EHR/Projects/post-covid-diabetes/results/model/AER/AER_compiled_results_extended.csv")
+df <- readr::read_csv("C:/Users/zy21123/OneDrive - University of Bristol/Documents - grp-EHR/Projects/post-covid-diabetes/results/model/AER//compiled_results_for_plotting/18_05_2023/AER_compiled_results.csv")
 
 #Select max day of follow up (196 for vax/unvax, 534 for pre-vax)
 # df <- df %>%
@@ -23,7 +24,7 @@ df$time_points <- NULL
 #Sum over all subgroups to get total
 df <- df %>% 
   group_by(event, cohort) %>%
-  summarise(total = sum(AER_main))
+  summarise(total = sum(AER))
 
 #Add person years of follow up
 table2_pre_vax <- read.csv("C:/Users/zy21123/OneDrive - University of Bristol/Documents - grp-EHR/Projects/post-covid-diabetes/results/descriptive/table2_prevax_diabetes.csv")

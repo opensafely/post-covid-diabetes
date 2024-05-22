f03fec43ed8a2f28ddf71c4f3cebb7143e3148e6
rdenholm <69469265+rdenholm@users.noreply.github.com>
2024-05-22 13:13:09 +0100

    Updating AIR table

--- analysis/figures/main/standardised_incidence_rates_table2.R
+++ analysis/figures/main/standardised_incidence_rates_table2.R
@@ -1,15 +1,11 @@
 # Developing standardised incidence rates for Table 2
 
 require(dplyr)
-
+require(tidyverse)
 
 # Specify directories -----------------------------------------------------------
 results_dir <- "C:/Users/rd16568/OneDrive - University of Bristol/grp-EHR/Projects/post-covid-diabetes/results"
 
-# Specify parameters -----------------------------------------------------------
-print('Specify parameters')
-
-perpeople <- 100000 # per X people
 
 # Load data --------------------------------------------------------------------
 print('Load data')
@@ -17,23 +13,37 @@ print('Load data')
 df <- read.csv(paste0(results_dir,"/model/AER/aer_input-main-rounded.csv"))
 
 df$outcome <- strsplit(df$outcome,'_extended_follow_up', fixed=TRUE) 
-
 df <- df[df$unexposed_person_days!=0,]
+df$outcome <- as.character(df$outcome)
+
+# Checking right number of events ------------------------------
+events <- aggregate(cbind(unexposed_events,exposed_events) ~ outcome + cohort, 
+                    data = df, 
+                    FUN = sum, na.rm = TRUE)
 
 # Specify reference data (prevax age and sex structure) --------------------------------------------------------------------
 print('Specify ref data')
 
-ref <- subset(df, select = c (aer_sex, aer_age, outcome, cohort, sample_size))
-ref <- ref[ref$cohort=="prevax" &
-           ref$outcome=="t1dm",]
-ref <- subset(ref, select = c (aer_sex, aer_age, sample_size))
+ref <- subset(df, select = c (aer_sex, aer_age, outcome, cohort, sample_size, unexposed_person_days))
+ref <- ref[ref$cohort=="prevax",]
+ref <- subset(ref, select = c (aer_sex, aer_age, outcome, unexposed_person_days))
 
-pop <- aggregate(x = ref$sample_size,                # Specify data column
-                    by = list(ref$aer_sex),          # Specify group indicator
+# Using unexposed person-days
+ref$unexposed_person_years <- ref$unexposed_person_days/365.25
+py <- aggregate(x = ref$unexposed_person_years,                # Specify data column
+                    by = list(ref$outcome),          # Specify group indicator
                     FUN = sum) 
 
-ref$pop <- pop$x[match(ref$aer_sex, pop$Group.1)]
-ref$per <- (ref$sample_size/ref$pop) # proportion of prevax cohort age and sex stratified
+ref$py <- py$x[match(ref$outcome, py$Group.1)]
+ref$py_per <- (ref$unexposed_person_years/ref$py) # proportion of prevax unexposed person-years, age and sex stratified
+
+# Using study population
+#pop <- aggregate(x = ref$sample_size,                # Specify data column
+#                    by = list(ref$aer_sex),          # Specify group indicator
+#                    FUN = sum) 
+
+#ref$pop <- pop$x[match(ref$aer_sex, pop$Group.1)]
+#ref$per <- (ref$sample_size/ref$pop) # proportion of prevax cohort age and sex stratified
 
 # Specify raw data --------------------------------------------------------------------
 print('Specify raw data')
@@ -59,13 +69,13 @@ raw <- raw %>% pivot_wider(names_from = c('cohort'),
                           values_from = 'IR')
 
 # Standardised IR data --------------------------------------------------------------------
-print('Calculate standardised incidence rate (Age and sex-specific IR multiplied by proportion in prevax age and sex strata)')
+print('Calculate standardised incidence rate (Age and sex-specific IR multiplied by proportion in prevax unexposed py age and sex strata)')
 
-sir <- merge(x=raw,y=ref,by=c("aer_sex", "aer_age"),all.x=TRUE)
+sir <- merge(x=raw,y=ref,by=c("aer_sex", "aer_age", "outcome"),all.x=TRUE)
 
-sir$vax_asir <- sir$vax * sir$per
-sir$unvax_asir <- sir$unvax * sir$per
-sir$prevax_asir <- sir$prevax * sir$per
+sir$vax_asir <- sir$vax * sir$py_per
+sir$unvax_asir <- sir$unvax * sir$py_per
+sir$prevax_asir <- sir$prevax * sir$py_per
 
 # sum over age and sex
 sir <- subset(sir, select = c (outcome, exposure, vax_asir, unvax_asir, prevax_asir))
